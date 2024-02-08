8dc173b8046a4b8af36b7a4b8dc3f7d599eb9c85
rajeshpaidipati <77273655+rajeshpaidipati@users.noreply.github.com>
2024-02-08 14:58:41 +0530

    First commit

---
+++ angular-figma-poc/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { FormTemplateComponent } from './form-template/form-template.component';
+
+export const routes: Routes = [
+    { path: 'form-page', component: FormTemplateComponent },
+];
