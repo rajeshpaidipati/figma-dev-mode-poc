d68d1df46a2209e77476dbd296d2d6ddfcf9d595
rajeshpaidipati <77273655+rajeshpaidipati@users.noreply.github.com>
2024-02-12 14:48:57 +0530

    Custom page completed

---
+++ angular-figma-poc/src/app/custom-form-template/custom-form-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-form-template',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-form-template.component.html',
+  styleUrl: './custom-form-template.component.css'
+})
+export class CustomFormTemplateComponent {
+
+}
